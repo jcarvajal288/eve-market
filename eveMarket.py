4a5cba60f5eebf81f6593c2752a632ba0cd44184
Max Carvajal <jcarvajal@crimson.ua.edu>
2015-03-02 00:52:42 -0600

    added csv with only market relevant typeIDs.  Rudimentary polling complete

--- eveMarket.py
+++ eveMarket.py
@@ -5,21 +5,24 @@ import xml.etree.ElementTree as et
 eveCentralEndpoint = "http://api.eve-central.com/api/marketstat"
 jitaID = 30000142
 
-def addItem(typeID, name):
+def getItemElement(typeID):
     query = "{0}?typeid={1}&usesystem={2}".format(eveCentralEndpoint, typeID, jitaID)
     req = requests.get(query)
     xmlRoot = et.fromstring(req.text)
-    print(xmlRoot.tag)
+    return xmlRoot
+    #print(req.text)
 
 
 def main():
-    products = []
+    items = []
     with open("typeids.csv", newline='') as typeidFile:
         typeids = csv.reader(typeidFile)
         for row in typeids:
-            products.append(addItem(row[2], row[1]))
+            items.append(getItemElement(row[0]))
+            print("added {}".format(row[1]))
+    print(len(items))
 
 
 if __name__ == "__main__":
-    #main()
-    addItem("16692", "name")
+    main()
+    #addItem("16692", "name")
