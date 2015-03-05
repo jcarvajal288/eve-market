03a3ef072fd0a5730894b56385e3e0f8218c98f2
Max Carvajal <jcarvajal@crimson.ua.edu>
2015-03-05 01:00:04 -0600

    added solar system csv and starting on system lookup logic

--- eveMarket.py
+++ eveMarket.py
@@ -1,3 +1,4 @@
+import argparse
 import concurrent.futures
 import csv
 import requests
@@ -7,8 +8,12 @@ eveCentralEndpoint = "http://api.eve-central.com/api/marketstat"
 jitaID = 30000142
 numRequests = 20
 
-def constructItemQuery(typeID):
-    query = "{0}?typeid={1}&usesystem={2}".format(eveCentralEndpoint, typeID, jitaID)
+def getSystemID(systemName):
+    pass
+
+def constructItemQuery(typeID, systemName):
+    systemID = getSystemID(systemName)
+    query = "{0}?typeid={1}&usesystem={2}".format(eveCentralEndpoint, typeID, systemID)
     return query
 
 def loadUrl(url):
@@ -30,14 +35,17 @@ def launchQuery(queries, results):
 def handleResults(marketResults):
     print("FINISHED: {} items polled".format(len(marketResults)))
 
-def main():
+def getSystemID(systemName):
+    pass
+
+def main(typeidFileName, system1, system2):
     marketResults = []
     with open("marketOnly_typeids.csv", newline='') as typeidFile:
         typeids = csv.reader(typeidFile)
         totalids = typeids.line_num
         urls = []
         for row in typeids:
-            urls.append(constructItemQuery(row[0]))
+            urls.append(constructItemQuery(row[0], system1))
             if len(urls) is numRequests:
                 launchQuery(urls, marketResults)
                 urls = []
@@ -46,4 +54,10 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    if sys.argc != 3:
+        print("Usage: python3 eveMarket.py [TYPEID_FILE] [SYSTEM 1] [SYSTEM 2]")
+    else:
+        typeidFile = sys.argv[1]
+        system1 = sys.argv[2]
+        system2 = sys.argv[3]
+        main(typeidFile, system1, system2)
